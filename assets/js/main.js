e3f8ef860aaed70e1b529fefbe551ec77f0ad3e0
fatimarajab12 <ahmadrajabfatima@gmail.com>
2024-05-22 21:04:54 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+// Show the button when the user scrolls down 20px from the top of the document
+window.onscroll = function() {
+    scrollFunction();
+  };
+  
+  function scrollFunction() {
+    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollToTopBtn.style.display = "block";
+    } else {
+      scrollToTopBtn.style.display = "none";
+    }
+  }
+  
+  // Scroll to the top of the document when the button is clicked
+  function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+  }
+  
